335e8afdfd9324564526110dcae2c5bcd0829631
usampvi <vigneshwaran.sampath@pearson.com>
2017-01-23 19:52:20 -0700

    recreated project with iPhone simulator support

---
+++ Weatherman/Weatherman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weatherman
+//
+//  Created by Sampath, Vigneshwaran on 1/23/17.
+//  Copyright © 2017 Weather Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
